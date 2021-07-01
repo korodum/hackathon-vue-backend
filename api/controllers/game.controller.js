c9ad09bae70eea483d2331d2ace5204f1463c9c1
Andrés Galván <adexuz7@gmail.com>
2021-07-01 12:21:46 +0100

    get game by id done

--- api/controllers/game.controller.js
+++ api/controllers/game.controller.js
@@ -6,3 +6,10 @@ exports.getAllGames = (req, res) => {
     .then(games => res.status(200).json(games))
     .catch(err => res.status(500).json(err))
 }
+
+exports.getOneGame = (req, res) => {
+  GameModel
+    .findById(req.params.gameId)
+    .then(game => res.status(200).json(game))
+    .catch(err => res.status(500).json(err))
+}
--- api/routes/games.router.js
+++ api/routes/games.router.js
@@ -1,9 +1,11 @@
 const gamesRouter = require('express').Router()
 const {
   getAllGames,
+  getOneGame,
 } = require('../controllers/game.controller')
 
 gamesRouter
   .get('/', getAllGames)
+  .get('/:gameId', getOneGame)
 
 exports.gamesRouter = gamesRouter
